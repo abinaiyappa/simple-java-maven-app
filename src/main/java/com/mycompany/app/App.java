467594ceb8ecc3d605d2cebd785c93aadb6b0a12
abinaiyappa <126042910+abinaiyappa@users.noreply.github.com>
2025-08-12 17:22:34 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! this is a testing 1";
+    private static final String MESSAGE = "Hello World! this is a testing 2";
 
     public App() {}
 
